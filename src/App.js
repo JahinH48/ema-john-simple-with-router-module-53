5091ae5d608e40223e9932feb440bde4c11da0ac
unknown <jahinhasan48@gmail.com>
2021-10-07 12:48:57 +0600

    jisan

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router ,Switch ,Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import OrderReview from './components/OrderReview/OrderReview';
+import Shop from './components/Shop/Shop';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Router> 
+
+        <Switch>
+          <Route exact path="/"> 
+          <Shop></Shop>
+          </Route>
+
+          <Route exact path="/shop"> 
+          <Shop></Shop>
+          </Route>
+
+          <Route  path="/review"> 
+          <OrderReview></OrderReview>
+          </Route>
+
+          <Route  path="/inventory"> 
+          <Inventory></Inventory>
+          </Route>
+
+          <Route  path="*"> 
+          <NotFound></NotFound>
+          </Route>
+
+
+        </Switch>
+
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
